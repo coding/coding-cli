3548ffe0d513f5c82d4b47981da31c4d7e5e16b2
sinkcup <sinkcup@gmail.com>
2021-07-07 21:26:47 +0800

    feat: #15 confluence HTML to markdown

--- tests/Unit/ConfluenceTest.php
+++ tests/Unit/ConfluenceTest.php
@@ -16,4 +16,11 @@ class ConfluenceTest extends TestCase
             'content' => '你好',
         ], $result);
     }
+
+    public function testHtmlFile2Markdown()
+    {
+        $confluence = new Confluence();
+        $markdown = $confluence->htmlFile2Markdown($this->dataDir . 'confluence/space1/text-demo_65601.html');
+        $this->assertEquals("你好\n==", $markdown);
+    }
 }
