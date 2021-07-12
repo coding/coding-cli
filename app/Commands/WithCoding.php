18948c2c66ebf532f64e55be071de891b3bbb3c3
sinkcup <sinkcup@gmail.com>
2021-07-12 19:09:51 +0800

    test: fix failed

--- app/Commands/WithCoding.php
+++ app/Commands/WithCoding.php
@@ -17,16 +17,18 @@ trait WithCoding
         if (is_null($codingTeamDomain)) {
             $codingTeamDomain = config('coding.team_domain') ?? $this->ask('CODING 团队域名：');
         }
-        $this->codingTeamDomain = str_replace('.coding.net', '', $codingTeamDomain);
+        $this->codingTeamDomain = str_replace('.coding.net', '', "$codingTeamDomain");
 
-        $this->codingProjectUri = $this->option('coding_project_uri');
-        if (is_null($this->codingProjectUri)) {
-            $this->codingProjectUri = config('coding.project_uri') ?? $this->ask('CODING 项目标识：');
+        $codingProjectUri = $this->option('coding_project_uri');
+        if (is_null($codingProjectUri)) {
+            $codingProjectUri = config('coding.project_uri') ?? $this->ask('CODING 项目标识：');
         }
+        $this->codingProjectUri = "$codingProjectUri";
 
-        $this->codingToken = $this->option('coding_token');
-        if (is_null($this->codingToken)) {
-            $this->codingToken = config('coding.token') ?? $this->ask('CODING Token：');
+        $codingToken = $this->option('coding_token');
+        if (is_null($codingToken)) {
+            $codingToken = config('coding.token') ?? $this->ask('CODING Token：');
         }
+        $this->codingToken = "$codingToken";
     }
 }
