9917bcff1edb79e5ab455665b74235b64474b03d
sinkcup <sinkcup@gmail.com>
2021-07-26 17:43:37 +0800

    fix: #40 env useless

--- config/coding.php
+++ config/coding.php
@@ -6,5 +6,7 @@ return [
     'project_uri' => env('CODING_PROJECT_URI'),
     'import' => [
         'provider' => env('CODING_IMPORT_PROVIDER'),
+        'data_type' => env('CODING_IMPORT_DATA_TYPE'),
+        'data_path' => env('CODING_IMPORT_DATA_PATH'),
     ],
 ];
