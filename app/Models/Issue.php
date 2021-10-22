f7e36923c7f6d9ce4522ab23f19bc0f556c1dda8
sinkcup <sinkcup@gmail.com>
2021-10-22 16:54:39 +0800

    refactor: #72 remove useless code

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -2,24 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Issue extends Model
+class Issue
 {
-    use HasFactory;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'type',
-        'name',
-        'priority',
-    ];
-
     public const PRIORITY_MAP = [
         '低' => '0',
         '中' => '1',
