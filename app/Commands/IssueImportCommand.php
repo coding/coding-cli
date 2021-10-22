12be361a8aada02cd6fb1a77256950f00dadb4be
sinkcup <sinkcup@gmail.com>
2021-10-22 16:54:39 +0800

    refactor: #72 remove useless code

--- app/Commands/IssueImportCommand.php
+++ app/Commands/IssueImportCommand.php
@@ -21,7 +21,6 @@ class IssueImportCommand extends Command
      */
     protected $signature = 'issue:import
         {file : 文件（支持格式：csv）}
-        {--type= : 类型（使用英文），如 DEFECT（缺陷）、REQUIREMENT（需求）、MISSION（任务）、EPIC（史诗）、SUB_TASK（子任务）}
         {--coding_token= : CODING 令牌}
         {--coding_team_domain= : CODING 团队域名，如 xxx.coding.net 即填写 xxx}
         {--coding_project_uri= : CODING 项目标识，如 xxx.coding.net/p/yyy 即填写 yyy}
